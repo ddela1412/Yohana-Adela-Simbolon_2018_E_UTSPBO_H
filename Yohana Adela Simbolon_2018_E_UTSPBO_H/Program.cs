c4b48f095a6b14c04667c23c76204789e158e0ac
Dela <252410102018@mail.unej.ac.id>
2026-05-22 19:46:19 +0700

    sddskjs

--- Yohana Adela Simbolon_2018_E_UTSPBO_H/Program.cs
+++ Yohana Adela Simbolon_2018_E_UTSPBO_H/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+// 1. Encapsulation
+// Membuat kelas ReservasiKamar yang memiliki atribut yang bersifat private
+// namaTamu, idBooking, tipeKamar
+new namaTamu = idnamaTamu
+new idBooking = idBooking
+new tipeKamar = tipeKamar
+
+
+public class ReservasiKamar;
+
+
+// 2. Inheritance
+// Membuat dua kelas turunan dari ReservasiKamar,
+// yakni KamarStandar & KamarSuite
+public class KamarStandar;
+public class KamarSuite;
+
+// 3. Polymorphism
+// Menambahkan metode hitungTotalBilling()
+public class hitungTotalBilling;
+
+// 4. Abstraction
+// Menjadikan kelas ReservasiKamar sebagai kelas abstrak & harus memiliki:
+// Metode abstrak hitungTotalBilling() & Metode umum tampilInfo()
+  
+
+// 5. Tambahan (menggunakan composition namun opsional saja)
+// Menambah metode tambahReservasi() dan cetakRiwayat
+public class RiwayatReservasi;
+
+
+
+
+
+
+
+
+
+
